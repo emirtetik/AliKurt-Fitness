e4a099784f5c91cf98571e980205e99cab70a777
Emir Tetik <info@frontmir.com>
2024-03-11 00:28:08 +0300

    metindeğişti

--- guides.js
+++ guides.js
@@ -5,6 +5,6 @@ export const guides = [
       title: "Geri Dönüş Programı",
       alt: 'ebook',
       pdf: '/images/GeriDönüşProgramı.pdf',
-      description: `Spora bir süre ara verdiniz ve kaybettiğiniz kasları olabildiğince hızlı, sürdürülebilir şekilde kazanmak mı istiyorsunuz? Öyleyse bu rehber ve antrenman programı tam size göre. Ayrıca, eğer kafa içi basıncınızı artırmadan antrenman yapma yükümlülüğünüz var ise (örn: Burun ameliyatı olmak ) endişelenmenize gerek yok, doğru adrestesiniz.`,
+      description: `Spora bir süre ara verdiniz ve kaybettiğiniz kasları olabildiğince hızlı, sürdürülebilir şekilde geri kazanmak mı istiyorsunuz? Öyleyse bu rehber ve antrenman programı tam size göre. Ayrıca, eğer kafa içi basıncınızı artırmadan antrenman yapma yükümlülüğünüz var ise (örn: Burun ameliyatı olmak ) endişelenmenize hiç gerek yok, doğru adrestesiniz`,
     },
   ];
